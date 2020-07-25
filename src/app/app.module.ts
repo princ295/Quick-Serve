a116563af0b76a801e2d8734d0d7a8d9277a0eff
princ295 <dewanganprince02@gmail.com>
2020-07-25 15:33:37 +0530

    login module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, AppRoutingModule, AuthRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
